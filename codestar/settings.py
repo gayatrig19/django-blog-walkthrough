ef29e39b82dac068f6eea096969b8a9e66ccb267
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-01-23 20:27:38 +0000

    generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-gayatrig19-djangoblogwa-maa8h92uetk.ws-eu107.gitpod.io', '.herokuapp.com']
 
