713ff29863cc2f83babccd22152d7c1cb5e0834c
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-01-25 19:42:09 +0000

    Add custom signup page for authorisation

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-gayatrig19-djangoblogwa-maa8h92uetk.ws-eu107.gitpod.io', '.herokuapp.com']
 
