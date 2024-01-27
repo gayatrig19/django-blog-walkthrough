a150486a555e77934a6224e3b68a75cbd05379e6
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-01-27 19:23:44 +0000

    Tidying up the code

--- about/admin.py
+++ about/admin.py
@@ -6,6 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
+    """
+    Adds rich-text editing of content in admin
+    """
 
     summernote_fields = ('content',)
 
@@ -18,6 +21,9 @@ class AboutAdmin(SummernoteModelAdmin):
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
+    """
+    Lists message and read fields for display in admin
+    """
 
     list_display = ('message', 'read',)
 
