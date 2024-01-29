d50ae921a3ede0a7ae952cd8fb73c3278d8e583c
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-01-29 19:58:51 +0000

    Change debug to false for deployment

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-
+# Register your models here.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
@@ -13,6 +13,16 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 
-# Register your models here.
 # admin.site.register(Post)
-admin.site.register(Comment)
+# admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('author', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
